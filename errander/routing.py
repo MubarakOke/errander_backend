799ef4794d28a85df7a0b3c9927abcf8e208fa37
MubarakOke <okeolamubarak18@gmail.com>
2022-03-10 05:23:00 -0800

    web socket setup

---
+++ errander/routing.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, re_path
+
+from operations.consumer import LocationConsumer
+
+websocket_urlpatterns = [
+    re_path(r'^order/(?P<id>\d+)/$', LocationConsumer.as_asgi()),
+   # path('order/<int:id>', LocationConsumer.as_asgi())
+]
--- errander/settings.py
+++ errander/settings.py
@@ -212,3 +212,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'errander.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
